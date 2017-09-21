93aab4ee39a5a40a79707085a7ef3396acedeede
CarmenIJsebaart <cijsebaart@gmail.com>
2017-09-21 20:20:08 +0200

    Can now move

--- main.cpp
+++ main.cpp
@@ -38,22 +38,6 @@ int main()
   view2.setViewport(sf::FloatRect(0.5f, 0, 0.5f, 1));
   view2.setSize(w_size_x/2, w_size_y);
 
-  sf::CircleShape RedRidingHood;
-  RedRidingHood.setRadius(10.0);
-  RedRidingHood.setFillColor(sf::Color::Red);
-  RedRidingHood.setPosition(sf::Vector2f(500, 500));
-
-  sf::CircleShape Wolf;
-  Wolf.setRadius(10.0);
-  Wolf.setFillColor(sf::Color(120, 120, 120));
-  Wolf.setPosition(sf::Vector2f(500, 500));
-
-  sf::RectangleShape eraser;
-  eraser.setOutlineColor(sf::Color::Yellow);
-  eraser.setFillColor(sf::Color::Transparent);
-  eraser.setOutlineThickness(2);
-  eraser.setSize(sf::Vector2f(w_size_x, w_size_y*2));
-
   sf::Texture grass;
   grass.loadFromFile("Grass.png");
   sf::Sprite map(grass);
@@ -62,6 +46,16 @@ int main()
   RRH.loadFromFile("CandlePicture_redhood.png");
   sf::Sprite RRH_sprite(RRH);
   RRH_sprite.setPosition(200, 200);
+
+  sf::Texture WOLF2;
+  WOLF2.loadFromFile("CandlePicture_wolf2.png");
+  sf::Sprite WOLF_sprite2(WOLF2);
+  WOLF_sprite2.setPosition(200, 200);
+
+  sf::Texture RRH2;
+  RRH2.loadFromFile("CandlePicture_redhood2.png");
+  sf::Sprite RRH_sprite2(RRH2);
+  RRH_sprite2.setPosition(200, 200);
 
   sf::Texture WOLF;
   WOLF.loadFromFile("CandlePicture_wolf.png");
@@ -79,7 +73,59 @@ int main()
         case sf::Event::Closed:
           w.close();
           break;
-        default:
+        case sf::Event::KeyPressed:
+          //Player 1
+          if(sf::Keyboard::isKeyPressed(sf::Keyboard::S))
+          {
+            view1.move(0, 1);
+            RRH_sprite.move(0, 1);
+            RRH_sprite2.move(0, 1);
+          }
+          if(sf::Keyboard::isKeyPressed(sf::Keyboard::D))
+          {
+            view1.move(1, 0);
+            RRH_sprite.move(1, 0);
+            RRH_sprite2.move(1, 0);
+          }
+          if(sf::Keyboard::isKeyPressed(sf::Keyboard::W))
+          {
+            view1.move(0, -1);
+            RRH_sprite.move(0, -1);
+            RRH_sprite2.move(0, -1);
+          }
+          if(sf::Keyboard::isKeyPressed(sf::Keyboard::A))
+          {
+            view1.move(-1, 0);
+            RRH_sprite.move(-1, 0);
+            RRH_sprite2.move(-1, 0);
+          }
+          //Player 2
+          if(sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
+          {
+            view2.move(0, 1);
+            WOLF_sprite.move(0, 1);
+            WOLF_sprite2.move(0, 1);
+          }
+          if(sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+          {
+            view2.move(1, 0);
+            WOLF_sprite.move(1, 0);
+            WOLF_sprite2.move(1, 0);
+          }
+          if(sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
+          {
+            view2.move(0, -1);
+            WOLF_sprite.move(0, -1);
+            WOLF_sprite2.move(0, -1);
+          }
+          if(sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+          {
+            view2.move(-1, 0);
+            WOLF_sprite.move(-1, 0);
+            WOLF_sprite2.move(-1, 0);
+          }
+          break;
+      default:
           break;
       }
     }
@@ -88,10 +134,12 @@ int main()
 
     w.setView(view1);
     w.draw(map);
+    w.draw(WOLF_sprite2);
     w.draw(RRH_sprite);
 
     w.setView(view2);
     w.draw(map);
+    w.draw(RRH_sprite2);
     w.draw(WOLF_sprite);
 
     w.display();
