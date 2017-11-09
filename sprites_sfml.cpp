9e795bbb9a509e02478c4ad1eef8438283aca89d
CarmenIJsebaart <cijsebaart@gmail.com>
2017-11-09 20:17:59 +0100

    Improved Sprite stuff

--- sprites_sfml.cpp
+++ sprites_sfml.cpp
@@ -1,7 +1,71 @@
 #include "sprites_sfml.h"
 
+#include "helper.h"
+
 sprites_sfml::sprites_sfml()
 {
+  //Create all resources needed: pictures, etc.
+  create_resources();
+
+  //Load player
+  {
+    sf::Texture * const texture = new sf::Texture;
+    texture->loadFromFile("CandlePicture_redhood.png");
+    m_RRH_sprite.setTexture(*texture);
+    m_textures.push_back(texture);
+  }
+  {
+    sf::Texture * const texture = new sf::Texture;
+    texture->loadFromFile("CandlePicture_redhood2.png");
+    m_RRH_sprite2.setTexture(*texture);
+    m_textures.push_back(texture);
+  }
+  {
+    sf::Texture * const texture = new sf::Texture;
+    texture->loadFromFile("CandlePicture_wolf.png.png");
+    m_wolf_sprite.setTexture(*texture);
+    m_textures.push_back(texture);
+  }
+  {
+    sf::Texture * const texture = new sf::Texture;
+    texture->loadFromFile("CandlePicture_wolf2.png");
+    m_wolf_sprite2.setTexture(*texture);
+    m_textures.push_back(texture);
+  }
+
+  //Load winner screens
+  {
+    sf::Texture * const texture = new sf::Texture;
+    texture->loadFromFile("Wolf_winner_screen.png");
+    m_wolf_wins_sprite.setTexture(*texture);
+    m_textures.push_back(texture);
+  }
+  {
+    sf::Texture * const texture = new sf::Texture;
+    texture->loadFromFile("deadgrandmother_winner_screen.png");
+    m_dead_grandmother_wolf_wins_sprite.setTexture(*texture);
+    m_textures.push_back(texture);
+  }
+  {
+    sf::Texture * const texture = new sf::Texture;
+    texture->loadFromFile("RRH_winner_screen.png");
+    m_RRH_wins_sprite.setTexture(*texture);
+    m_textures.push_back(texture);
+  }
+
+  ///Other pictures
+  {
+    sf::Texture * const texture = new sf::Texture;
+    texture->loadFromFile("Grandmother.png");
+    m_grandmother_sprite.setTexture(*texture);
+    m_textures.push_back(texture);
+  }
+  {
+    sf::Texture * const texture = new sf::Texture;
+    texture->loadFromFile("Hunter.png");
+    m_hunter_sprite.setTexture(*texture);
+    m_textures.push_back(texture);
+  }
 
 }
 
--- sprites_sfml.h
+++ sprites_sfml.h
@@ -1,11 +1,48 @@
 #ifndef SPRITES_SFML_H
 #define SPRITES_SFML_H
 
+#include <SFML/Graphics.hpp>
 
 class sprites_sfml
 {
 public:
+  //Prepare all sprites
   sprites_sfml();
+
+  sf::Sprite& get_RRH_sprite() noexcept { return m_RRH_sprite; }
+  sf::Sprite& get_RRH_sprite2() noexcept { return m_RRH_sprite2; }
+  sf::Sprite& get_wolf_sprite() noexcept { return m_wolf_sprite; }
+  sf::Sprite& get_wolf_sprite2() noexcept { return m_wolf_sprite2; }
+  sf::Sprite& get_wolf_wins_sprite() noexcept { return m_wolf_wins_sprite; }
+  sf::Sprite& get_dead_grandmother_wolf_wins() noexcept { return m_dead_grandmother_wolf_wins_sprite; }
+  sf::Sprite& get_RRH_wins() noexcept { return m_RRH_wins_sprite; }
+  sf::Sprite& get_grandmother() noexcept { return m_grandmother_sprite; }
+  sf::Sprite& get_hunter() noexcept { return m_hunter_sprite; }
+
+private:
+
+  ///Pictures of players
+  sf::Sprite m_RRH_sprite;
+  sf::Sprite m_RRH_sprite2;
+  sf::Sprite m_wolf_sprite;
+  sf::Sprite m_wolf_sprite2;
+
+  ///Winner screen pictures
+  sf::Sprite m_wolf_wins_sprite;
+  sf::Sprite m_dead_grandmother_wolf_wins_sprite;
+  sf::Sprite m_RRH_wins_sprite;
+
+  ///Other pictures
+  sf::Sprite m_grandmother_sprite;
+  sf::Sprite m_hunter_sprite;
+
+  ///Textures
+  std::vector<sf::Texture*> m_textures;
+
 };
+
+std::ostream& operator<<(std::ostream &os, const sprites_sfml&) noexcept;
+///Will always return true, because this are the one and only sprites.
+bool operator==(const sprites_sfml&, const sprites_sfml&) noexcept;
 
 #endif // SPRITES_SFML_H
